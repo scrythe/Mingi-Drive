df05d493d5d4851eefdbafeb93f665194d59f78f
Magomed Alimkhanov <scrythepvp@gmail.com>
2024-07-03 00:23:36 +0200

    feat: add a bit component entity stuff

--- src/game.ts
+++ src/game.ts
@@ -2,12 +2,31 @@ import "./style.css";
 import * as THREE from "three";
 
 type QueryFunction = (query: string) => void;
+class Component { }
+
+class Entity {
+  components: Map<string, Component>;
+
+  constructor() {
+    this.components = new Map();
+  }
+
+  addComponent(component: Component) {
+    const componentType = component.constructor.name;
+    this.components.set(componentType, component);
+  }
+
+  getComponent<T extends Component>(componentType: new () => T) {
+    return this.components.get(componentType.name) as T | undefined;
+  }
+}
 
 class App {
   scene: THREE.Scene;
   camera: THREE.Camera;
   renderer: THREE.WebGLRenderer;
   systems: QueryFunction[];
+  entities: Entity[];
 
   constructor() {
     this.scene = new THREE.Scene();
@@ -19,6 +38,8 @@ class App {
     );
     this.renderer = new THREE.WebGLRenderer();
     this.systems = [];
+    this.entities = [];
+
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setAnimationLoop(() => this.update());
     document.body.appendChild(this.renderer.domElement);
@@ -28,17 +49,33 @@ class App {
     for (const system of this.systems) {
       system("hm");
     }
+    for (const entity of this.entities) {
+      for (const component of entity.components.values()) {
+        console.log(component);
+      }
+    }
     this.renderer.render(this.scene, this.camera);
   }
 
   addSystem(system: QueryFunction) {
     this.systems.push(system);
   }
+
+  addEntity(entity: Entity) {
+    this.entities.push(entity);
+  }
 }
+
+class Person { }
+class Name { }
 
 function main() {
   const app = new App();
   app.addSystem((hm: string) => console.log(hm));
+  const newEntity = new Entity();
+  newEntity.addComponent(new Person());
+  newEntity.addComponent(new Name());
+  app.addEntity(newEntity);
 }
 
 main();
