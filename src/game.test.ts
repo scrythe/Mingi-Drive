55d89911094b90f1d31db7ff6c9ff33306bfb45f
Magomed Alimkhanov <scrythepvp@gmail.com>
2024-08-02 14:55:45 +0200

    feat: add debugging

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,8 +1,10 @@
 // sum.test.js
-// import { expect, test } from "vitest";
-// import { components, defineComponent, types } from "./game2";
-//
-// test("components correct", () => {
-//   defineComponent({ name: "hm", type: types.i8 });
-//   expect(components).toEqual([{ name: "hm", type: Int8Array }]);
-// });
+import { expect, test } from "vitest";
+// import { } from "./game";
+
+test("components correct", () => {
+  // defineComponent({ name: "hm", type: types.i8 });
+  // expect(components).toEqual([{ name: "hm", type: Int8Array }]);
+  console.log("hmm")
+  expect("hm").toEqual("hm");
+});
